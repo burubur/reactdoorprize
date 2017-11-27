4c17d0fe4db830ac2f4d6a67d0d34d453e7431a1
Burhan Mubarok <burhanmubarok@gmail.com>
2017-11-27 22:33:13 +0700

    init html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const HtmlPlugin = require('html-webpack-plugin')
+
 const config = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlPlugin({
+      title: 'Doorprize'
+    })
+  ]
 }
 
 module.exports = config
