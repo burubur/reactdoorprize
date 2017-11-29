4639085ec9cb82c9a617a145cad9cc76a7a406dd
Burhan Mubarok <burhanmubarok@gmail.com>
2017-11-30 00:04:49 +0700

    apply layout to app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Base from '../layouts/base'
+import './app.less'
 
 export default class App extends React.Component {
   render () {
     return (
-      <div>App</div>
+      <div>
+        <Base />
+      </div>
     )
   }
 }
