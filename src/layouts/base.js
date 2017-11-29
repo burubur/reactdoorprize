d28e71f1804211743f5893713befbe1a306671a0
Burhan Mubarok <burhanmubarok@gmail.com>
2017-11-30 00:04:31 +0700

    create base layout

---
+++ src/layouts/base.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from './navigation'
+
+export default class Base extends React.Component {
+  render() {
+    return(
+      <div>
+        <Navigation />
+      </div>
+    )
+  }
+}
