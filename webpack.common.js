793ed9ad6acf26c23d2a7f913aee0aefa58154f9
Burhan Mubarok <burhanmubarok@gmail.com>
2017-11-30 00:03:15 +0700

    install less loader

--- webpack.common.js
+++ webpack.common.js
@@ -36,6 +36,17 @@ module.exports = {
             loader: 'css-loader'
           }
         ]
+      }, {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader'
+          }
+        ]
       }
     ]
   }
